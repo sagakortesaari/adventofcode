b8528aed7a11e02df28602a399b0bb73d97e2f58
Saga Kortesaari <sagak@student.chalmers.se>
2020-12-01 06:42:03 +0100

    day1 done :)

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,21 @@
 f = open("input.txt", "r")
 lines = f.readlines()
-lines = [line.strip("\n") for line in lines]
+lines = [int(line.strip("\n")) for line in lines]
 
+#part 2
+def multiplyThree():
+    for l0 in lines:
+        for l1 in lines: 
+            for l2 in lines:
+                if l0 + l1 + l2 == 2020:
+                    return(l0 * l1 * l2)
+
+#part 1
+def multiplyTwo():
+    for line in lines:
+        remainder = 2020 - int(line)
+        if remainder in lines: 
+            return remainder * line
+
+print(multiplyTwo())
+print(multiplyThree())
