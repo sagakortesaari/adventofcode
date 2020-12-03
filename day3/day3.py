c7b9886780fbf2ff73ce3680779f654caf932fef
Saga Kortesaari <sagak@student.chalmers.se>
2020-12-03 08:18:49 +0100

    day3 done :)

---
+++ day3/day3.py
@@ -0,0 +1,39 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+lines = [line.strip() for line in lines]
+
+def day_3(right, downtwo):
+    trees = 0
+    x = 0
+    everyother = True
+    for line in lines:
+        if not everyother and downtwo:
+            everyother = True 
+            continue
+        if line[x] == "#":
+            trees += 1
+        x += right 
+        if x >= len(line):
+            x = x % len(line)
+        everyother = False
+    return trees
+
+
+right1 = day_3(1, False)
+right3 = day_3(3, False)
+right5 = day_3(5, False)
+right7 = day_3(7, False)
+right1down2 = day_3(1, True)
+print(right1*right3*right5*right7*right1down2)
+
+
+        
+
+            
+            
+            
+
+            
+
+
+
