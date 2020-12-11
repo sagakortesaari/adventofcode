157778c37e60042a12aa08be978ae4a05baf2d2b
Saga Kortesaari <saga.kortesaari@hotmail.com>
2020-12-12 00:40:47 +0100

    day10 part 2

--- day10/day10.py
+++ day10/day10.py
@@ -1,6 +1,7 @@
 f = open("input.txt", "r")
 lines = f.readlines()
 lines = [int(line.strip()) for line in lines]
+alreadycalculated = {}
 
 def part_1():
     adapter_jolts = sorted(lines)
@@ -20,3 +21,26 @@ def part_1():
     return onejoltdiff * (threejoltdiff + 1)
 
 print(part_1())
+
+def part_2(adapter_jolts, max_distance = 3, curr = 0):
+    if curr in alreadycalculated:
+        return alreadycalculated[curr]
+
+    if len(adapter_jolts) == 1:
+        return 2
+
+    elif len(adapter_jolts) == 0:
+        return 0
+
+    valids = 0
+
+    withinrange = [adapter for adapter in adapter_jolts if adapter - curr <= 3]
+
+    for elem in withinrange:
+        valids += part_2(adapter_jolts[adapter_jolts.index(elem)+1:], curr = elem)
+    
+    alreadycalculated[curr] = valids
+    
+    return valids
+
+print(part_2(sorted(lines)))
